ec3d52d20d00f1a1ca2083651e80daf4d8c9eb31
chubbell <chubbell@oregonstate.edu>
2019-09-15 21:26:46 +0700

    new Pi code

--- Receiver/Receiver Code/Pi_LoRa_Working.py
+++ Receiver/Receiver Code/Pi_LoRa_Working.py
@@ -14,7 +14,7 @@ import csv
 import threading
 
 new_file = True
-tran_nuber = 0 # shows not real transmitter
+tran_number = 0 # shows not real transmitter
 fieldnames=['Date/time', 'Temp', 'Humidity', 'Air pressure', 'Lux', 'NO2', 'CH4', 'NH3']
 
 # make the port
@@ -50,7 +50,7 @@ time.sleep(3)
 
 # look for valid response
 for i in range(3):
-    #print(response + str(i))
+    #print(response)
     response = port.read(3)
 #print ("this one:" + response[1:3])
 
@@ -62,8 +62,8 @@ if response[1:3] == "OK":
    port.write("AT+NA=00112233\n".encode()) # sets network address
    port.write("AT+NSK=00112233001122330011223300112233\n".encode()) # sets network session key
    port.write("AT+DSK=33221100332211003322110033221100\n".encode()) # sets data session key
-   port.write("AT+TXDR=DR3\n".encode()) # sets the transmit data rate (AS 923) //TODO, adjust to get better range!
-   port.write("AT+TXF=920000000\n".encode()) # sets the transmit frequency (920000000 - 928000000) //TODO, adjust to get better range!
+   port.write("AT+TXDR=DR0\n".encode()) # sets the transmit data rate (AS 923) //TODO, adjust to get better range!
+   port.write("AT+TXF=921000000\n".encode()) # sets the transmit frequency (920000000 - 928000000) //TODO, adjust to get better range!
    port.write("AT+ACK=8\n".encode())
    
    # use to write settings to flash
@@ -87,51 +87,61 @@ sendit()
 # infinite loop!
 while True:
     # Mdot is already configured, so can just start receiving in a loop with a delay, to see if sender is sending
-    response = port.readline()
+    response = port.readline().decode()
     #port.write(time.strftime("%m/%e/%G %H:%M:%S"))
     #port.write("\n")
+    print(response)
     
     #test script below
     #response = "01/01/1997 06:30:23,10,14,19,70,0,60"
     if response:
-        print("Received at " + time.strftime("%H:%M:%S")
-               + " --> " + response) # for debugging
+        #print("Received at " + time.strftime("%H:%M:%S")
+        #       + " --> " + response) # for debugging
         #port.write(time.strftime("%m/%e/%G %H:%M:%S")+ "\n")
         
         # perform handshake and give teensy the time
-        if response == "Time?":
-            print("hand shook with: " + response)
-            print("time:" + time.strftime("%m/%e/%G %H:%M:%S"))
-            port.write(time.strftime("%m/%e/%G %H:%M:%S") + "\n")
+        if "Time?" in response:
+            print("hand shook")
+            #print("time:" + time.strftime("%m/%e/%G %H:%M:%S"))
+            #port.write("hello\r\n".encode())
+            #port.write(time.strftime("%m/%e/%G %H:%M:%S").encode())
+            #port.write("09/14/2019 14:22:52\n".encode())
+            port.write((time.strftime("%m/%e/%G %H:%M:%S") + "\n").encode())
+            time.sleep(3)
+            port.read()
 
         #parse data into array
         data = response.rstrip().split(",")
         print(data) # for debug
         
         # extract the tran number, and then dont care
-        tran_number = data[0]
-        data.remove(0)
+        try:
+            tran_number = int(data[0])
+            del data[0]
+        except:
+            tran_number = 0 #this is bad data
         
-        #write data if it is valid
-        if (len(data) > 5):
-            if(new_file == True):
-                toOpen = "/home/pi/Documents/Data/data"+str(tran_number)+".csv"
-                file1 = open(toOpen, 'w')
+        if (tran_number > 0 and tran_number < 1):
+            #write data if it is valid
+            if (len(data) > 5):
+                if(new_file == True):
+                    toOpen = "/home/pi/Documents/Data/data"+str(tran_number)+".csv"
+                    file1 = open(toOpen, 'w+')
+                    with file1 as csvfile:
+                        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                        print("New file...writing header")
+                        writer.writeheader()
+                    new_file = False
+                else:
+                    toOpen = "/home/pi/Documents/Data/data"+str(tran_number)+".csv"    
+                    print("file being written to: " +str(tran_number))
+                #toOpen = "/home/pi/Documents/ECE44x/Wireless/data"+str(tran_number[0]+1)+".csv"
+                file1 = open(toOpen, 'a+')
                 with file1 as csvfile:
+                    print("Received valid data: " + str(data))
+                    #print("lux: " + data[4])
                     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                    print("New file...writing header")
-                    writer.writeheader()
-                new_file = False
-            else:
-                toOpen = "/home/pi/Documents/Data/data"+str(tran_number)+".csv"    
-                print("file being written to: " +str(tran_number))
-            #toOpen = "/home/pi/Documents/ECE44x/Wireless/data"+str(tran_number[0]+1)+".csv"
-            file1 = open(toOpen, 'a')
-            with file1 as csvfile:
-                print("Received valid data: " + str(data))
-                #print("lux: " + data[4])
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writerow({fieldnames[0]: data[0], fieldnames[1]: data[1], fieldnames[2]: data[2],
-                                fieldnames[3]: data[3], fieldnames[4]: data[4], fieldnames[5]:data[5],
-                                fieldnames[6]: data[6]})
-                #file1.close()
+                    writer.writerow({fieldnames[0]: data[0], fieldnames[1]: data[1], fieldnames[2]: data[2],
+                                    fieldnames[3]: data[3], fieldnames[4]: data[4], fieldnames[5]:data[5],
+                                    fieldnames[6]: data[6]})
+                    #file1.close()
